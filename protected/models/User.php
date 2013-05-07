314757e6bcca2d58cccc0b31a410c2fb32a4ccd9
Admin <Admin@192.168.71.195>
2013-05-07 16:33:39 -0400

    user name

--- protected/models/User.php
+++ protected/models/User.php
@@ -7,4 +7,24 @@ class User extends BaseUser
 	public static function model($className=__CLASS__) {
 		return parent::model($className);
 	}
+        
+        public function attributeLabels() {
+		return array(
+			'id' => Yii::t('app', 'ID'),
+			'user_id' => Yii::t('app', 'Cliente'),
+			'username' => Yii::t('app', 'Empleado'),
+			'password' => Yii::t('app', 'Password'),
+			'estado' => Yii::t('app', 'Estado'),
+			'salt' => Yii::t('app', 'Salt'),
+			'tipo' => Yii::t('app', 'Tipo'),
+			'entel' => Yii::t('app', 'Entel'),
+			'movistar' => Yii::t('app', 'Movistar'),
+			'atencions' => null,
+			'estados' => null,
+			'locals' => null,
+			'recargas' => null,
+			'user' => null,
+			'users' => null,
+		);
+	}
 }
